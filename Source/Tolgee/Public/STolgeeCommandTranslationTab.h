d045aa18bf90ab3639835300bb45445825910752
Alexandru Oprea <alexandru@outofthebox-plugins.com>
2026-07-09 11:02:49 +0300

    Runtime version of the translation tab

---
+++ Source/Tolgee/Public/STolgeeCommandTranslationTab.h
@@ -0,0 +1,32 @@
+// Copyright (c) Tolgee 2022-2025. All Rights Reserved.
+
+#pragma once
+
+#include "STolgeeTranslationTab.h"
+
+class STolgeeCommandTranslationTab : public STolgeeTranslationTab
+{
+public:
+	SLATE_BEGIN_ARGS(STolgeeCommandTranslationTab) { }
+		SLATE_ARGUMENT(FString, BaseUrl)
+		SLATE_ARGUMENT(FString, ApiKey)
+		SLATE_ARGUMENT(FString, ProjectId)
+	SLATE_END_ARGS()
+
+	/**
+	* @brief Constructs the translation dashboard widget for the console command
+	*/
+	void Construct(const FArguments& InArgs);
+
+protected:
+	//~Begin STolgeeTranslationTab interface
+	virtual FString GetBaseUrl() const override;
+	virtual FString GetApiKey() const override;
+	virtual TArray<FString> GetProjectIds() const override;
+	//~End STolgeeTranslationTab interface
+
+private:
+	FString BaseUrl;
+	FString ApiKey;
+	FString ProjectId;
+};
